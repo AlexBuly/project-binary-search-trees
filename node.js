6e9a32de82176107ae8888a6dfe35a27152bf87b
Alex Buly <asbuly@icloud.com>
2024-12-29 20:06:40 -0500

    Adds initialpsuedocode

--- node.js
+++ node.js
@@ -1,7 +1,15 @@
-class Node {
+export class Node {
     constructor(data) {
         this.data = data;
         this.left = null;
         this.right = null;
+    }
+
+    setLeft(left) {
+        this.left = left;
+    }
+
+    setRight(right) {
+        this.right = right;
     }
 }
--- tree.js
+++ tree.js
@@ -2,4 +2,18 @@ class Tree {
     constructor() {
         this.root = null;
     }
+
+    buildTree(array) {
+        /*
+
+        if (start > end) return null 
+        let mid = (start + end) / 2
+        let root = new Node(mid)
+
+        root.setLeft(left half)
+        root.setRight(right half)
+        
+        return root
+        */
+    }
 }
