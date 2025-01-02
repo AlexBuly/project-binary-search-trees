89b0971c63d0188e5504266efa7e7034bf84929b
Alex Buly <asbuly@icloud.com>
2025-01-02 17:06:14 -0500

    Adds sorting

--- tree.js
+++ tree.js
@@ -1,19 +1,34 @@
-class Tree {
+import { Node } from "./node.js";
+import { mergeSort } from "./mergeSort.js";
+
+export class Tree {
     constructor() {
         this.root = null;
     }
 
     buildTree(array) {
-        /*
+        array = mergeSort(array);
+        if (array.length <= 1) return array;
+        let mid = Math.floor(array.length / 2);
+        let node = new Node(array[mid]);
+        node.setLeft(this.buildTree(array.slice(0, mid)));
+        node.setRight(this.buildTree(array.slice(mid+1)));
+        this.root = node;
+        return this.root;
+    }
 
-        if (start > end) return null 
-        let mid = (start + end) / 2
-        let root = new Node(mid)
+    prettyPrint(node, prefix = "", isLeft = true) {
+        node = new Node(this.root);
 
-        root.setLeft(left half)
-        root.setRight(right half)
-        
-        return root
-        */
+        if (node === null) {
+            return;
+          }
+          if (node.right !== null) {
+            prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+          }
+          console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+          if (node.left !== null) {
+            prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+          }
     }
 }
