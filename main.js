8a949ec68ea7e775b8f6c6c0894c07595cde8ee3
Alex Buly <asbuly@icloud.com>
2025-01-10 17:25:41 -0500

    Completes buildTree and prettyPrint functions

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import { Tree } from "./tree.js";
 
 const tree = new Tree();
 const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const build = tree.buildTree(array);
+const uniqueArray = [...new Set(array)];
+const build = tree.buildTree(uniqueArray);
 console.log(build);
-tree.prettyPrint(build);
+console.log(tree.getSize());
+tree.prettyPrint();
--- tree.js
+++ tree.js
@@ -4,34 +4,78 @@ import { mergeSort } from "./mergeSort.js";
 export class Tree {
     constructor() {
         this.root = null;
+        this.size = 0;
     }
 
-    buildTree(array) {
-        array = mergeSort(array);
-       const set = new Set(array);
-       let setVals = [...set];
-       array = setVals;
-        if (array.length <= 1) return array;
-        let mid = Math.floor(array.length / 2);
+    buildTree(array, start = 0, end = array.length - 1) {
+       array = mergeSort(array);
+       if (start > end) return null;
+       let mid = Math.floor((start + end) / 2);
         let node = new Node(array[mid]);
-        node.setLeft(this.buildTree(array.slice(0, mid)));
-        node.setRight(this.buildTree(array.slice(mid+1)));
+        node.setLeft(this.buildTree(array, start, mid - 1));
+        node.setRight(this.buildTree(array, mid + 1, end));
+        this.size++;
         this.root = node;
         return this.root;
     }
 
-    prettyPrint(node, prefix = "", isLeft = true) {
-        node = new Node(this.root);
+    prettyPrint(node = this.root, prefix = "", isLeft = true) {
 
         if (node === null) {
             return;
           }
           if (node.right !== null) {
-            prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+            // if current is a left child, add | to prefix. If right, add " " 
+            this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
           }
+        
           console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+      
           if (node.left !== null) {
-            prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+            this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
           }
     }
+
+    insert(value, current = this.root) {
+
+      let newNode = new Node(value);
+
+      if (this.root == null) this.root = newNode;
+   
+      if (current.left == null && current.right == null) return current;
+
+      if (newNode.data < current.data) { 
+        const insert = this.insert(value, current = current.left);
+        current.setLeft(newNode);   
+   
+        this.size++;  
+        return insert;
+
+      } else if (newNode.data > current.data) {
+          const insert = this.insert(value, current = current.right);
+          current.setRight(newNode);
+          this.size++;
+          return insert;
+      }
+    }
+
+    deleteItem(value) {
+      /*
+        // if leaf
+        if (value.left == null && value.right == null) {
+          prev.left or prev.right = null
+          // if node has one child
+        } else if (value.left || value.right) {
+            prev.next = current.next
+         } else if (value.left && value.right) {
+            find second largest from node 
+            replace node with second largest node
+          }
+      */
+    }
+
+    getSize() {
+      return this.size;
+    }
+     
 }
